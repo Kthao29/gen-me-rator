3ab47cb3a84a452a7885fb20c53db686c2ae2571
Keng <kengthao57@gmail.com>
2023-11-08 20:18:02 -0600

    Added video

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
     if (!license) {
       return ``;
@@ -14,8 +13,7 @@ function renderLicenseBadge(license) {
  } 
 }
   
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function that returns the license link
 function renderLicenseLink(license) {
     if (license === 'MIT') {
       return `https://www.mit.edu/~amini/LICENSE.md`;
@@ -33,8 +31,7 @@ function renderLicenseLink(license) {
 
 
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function that returns the license section of README
 function renderLicenseSection(license) {
     if (!license) {
       return ``;
@@ -45,7 +42,7 @@ function renderLicenseSection(license) {
   }
   
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
